76225b29ba1d1660038950f24150ee2c27946351
Mattias Gaspar <mattiasmac2000@gmail.com>
2023-02-06 09:04:51 -0300

    update

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { get } from 'lodash';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import { Container } from '../../styles/GlobalStyles';
+import { AlunoContainer, ProfilePicture } from './styled';
+// import axios from '../../services/axios';
 
 export default function Alunos() {
+  // eslint-disable-next-line no-unused-vars
+  const [alunos, setAlunos] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      // const response = await axios.get('/alunos');
+      const response = [
+        { id: 1, nome: 'test1', email: 'asdasdasd@asd.com', Fotos: [] },
+        { id: 2, nome: 'test2', email: 'asdasdasd@asd.com', Fotos: [] },
+      ];
+      // setAlunos(response.data);
+
+      setAlunos(response);
+    }
+
+    getData();
+  }, []);
   return (
     <Container>
       <h1>Alunos</h1>
+
+      <AlunoContainer>
+        {alunos.map((aluno) => {
+          return (
+            <div key={String(aluno.id)}>
+              <ProfilePicture>
+                {get(aluno, 'Fotos[0].url', false) ? (
+                  <img src={aluno.Fotos[0].url} alt="" />
+                ) : (
+                  <FaUserCircle size={36} />
+                )}
+              </ProfilePicture>
+
+              <span>{aluno.nome}</span>
+              <span>{aluno.email}</span>
+
+              <Link to={`/aluno/${aluno.id}/edit`}>
+                <FaEdit size={16} />
+              </Link>
+
+              <Link to={`/aluno/${aluno.id}/edit`}>
+                <FaWindowClose size={16} />
+              </Link>
+            </div>
+          );
+        })}
+      </AlunoContainer>
     </Container>
   );
 }
