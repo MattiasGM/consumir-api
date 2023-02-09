46188c2a98edb4917ac3c1ac47291383c9396d62
Mattias Gaspar <mattiasmac2000@gmail.com>
2023-02-09 18:43:13 -0300

    update

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
 
 import { Container } from '../../styles/GlobalStyles';
 
-export default function Aluno() {
+export default function Aluno({ match }) {
+  const id = get(match, 'params.id', 0);
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
   return (
     <Container>
-      <h1>Aluno</h1>
+      <h1>{id ? 'Editar Aluno' : 'Novo Aluno'}</h1>
     </Container>
   );
 }
+
+Aluno.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
